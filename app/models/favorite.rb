1d58f0e7feca5ea0f6c8c1f37412d9fc8b832c50
Abby Farrell <abbykass@gmail.com>
2014-05-26 19:52:34 -0500

    added first round of association methods

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,7 @@
 class Favorite < ActiveRecord::Base
+
+belongs_to :photo
+belongs_to :user
+
+
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,2 +1,8 @@
 class Photo < ActiveRecord::Base
+
+belongs_to :user
+has_many :favorites
+has_many :comments
+
+
 end
